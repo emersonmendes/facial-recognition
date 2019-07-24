2508e781dad809fca485d0a2248ef222a5e0313a
Emerson Mendes <emerson.mendes@invillia.com>
2019-07-23 21:42:08 -0300

    melhoria

--- src/faces.py
+++ src/faces.py
@@ -14,26 +14,24 @@ def process_frames(cap):
     ret, original_frame = cap.read()
     gray_frame = cv.cvtColor(original_frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.5, minNeighbors=5)
-    for(x, y, w, h) in faces:
-        draw_data(x, y, w, h, gray_frame, original_frame)
-    # display result frame
-    cv.imshow('frame', original_frame)
-
-def draw_data(y, x, h, w, gray_frame, original_frame):
     
-    cord_y = y + h
-    cord_x = x + w
-    region_of_interest = gray_frame[y:cord_y, x:cord_x]
+    for(x, y, w, h) in faces:
+        cord_y = y + h
+        cord_x = x + w
+        region_of_interest = gray_frame[y:cord_y, x:cord_x]
 
-    id_, confidence = recognizer.predict(region_of_interest)
-    if(confidence > 60):
-        name = labels[id_]
-        color = (255,255,255)
-        label = "{}: {:.2f}%".format(name, confidence)
-        cv.putText(original_frame, label, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv.LINE_AA )
+        id_, confidence = recognizer.predict(region_of_interest)
+        if(confidence >= 60 and confidence <= 85):
+            name = labels[id_]
+            color = (255,255,255)
+            label = "{}: {:.2f}%".format(name, confidence)
+            cv.putText(original_frame, label, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv.LINE_AA )
 
-    # draw rectangle
-    cv.rectangle(original_frame, (x, y), (cord_x, cord_y), (255,50,150), 2)
+        # draw rectangle
+        cv.rectangle(original_frame, (x, y), (cord_x, cord_y), (255,50,150), 2)
+
+    # display result frame
+    cv.imshow('frame', original_frame)  
 
 def init():
     cap = cv.VideoCapture(0)
