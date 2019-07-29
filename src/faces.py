7e355d9c776979e1a6fcefe42c9d84498824be5a
Emerson Mendes <emerson.emendes@gmail.com.br>
2019-07-29 11:39:15 -0300

    x

--- src/faces.py
+++ src/faces.py
@@ -1,10 +1,10 @@
 """..."""
 
-import cv2
 import os
 import pickle
-import imutils
 import time
+import cv2
+import imutils
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 face_cascade = cv2.CascadeClassifier(os.path.join(BASE_DIR, 'cascades/lbpcascade_frontalface.xml'))
@@ -12,42 +12,38 @@ labels = {}
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("data/trainer.yml")
 
-with open("data/labels.pickle", 'rb') as f: 
+with open("data/labels.pickle", 'rb') as f:
     labels = {v:k for k, v in pickle.load(f).items()}
 
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("/home/emerson/Downloads/xx.mp4")
 
-while(True):
+while True:
 
     # capture frame by frame
-    ret, original_frame = cap.read()
-    original_frame  = imutils.resize(original_frame, width=550)
+    ret, frame = cap.read()
+    frame = imutils.resize(frame, width=550)
 
-    gray_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY)   
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
     #faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.2, minNeighbors=5)
     faces = face_cascade.detectMultiScale(gray_frame)
 
-    if (len(faces) == 0):
-        continue
-    
     for(x, y, w, h) in faces:
-        cv2.rectangle(original_frame, (x, y), (x+w, y+h), (255,50,150), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 50, 150), 2)
         region_of_interest = gray_frame[y:y+h, x:x+w]
         id_, confidence = recognizer.predict(region_of_interest)
         if(confidence >= 99 and confidence <= 100):
             name = labels[id_]
             label = "{}: {:.2f}%".format(name, confidence)
-            cv2.putText(original_frame, label, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (255,255,255), 2)
+            cv2.putText(frame, label, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 255, 255), 2)
             print('label: ' + label)
-            time.sleep(5) 
+            time.sleep(5)
 
-    cv2.imshow('frame', original_frame) 
-
+    cv2.imshow('frame', frame)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
