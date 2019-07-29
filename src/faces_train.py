e4da70747c4c125314ecd5be7174b6ef026f5d75
Emerson Mendes <emerson.emendes@gmail.com.br>
2019-07-29 12:03:58 -0300

    x

--- src/faces_train.py
+++ src/faces_train.py
@@ -8,48 +8,50 @@ from PIL import Image
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(base_dir, "images")
+frontalface = os.path.join(base_dir, 'cascades/lbpcascade_frontalface.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 y_labels = []
 x_train = []
 
-current_id = 0
 label_ids = {}
 
-face_cascade = cv2.CascadeClassifier(os.path.join(base_dir, 'cascades/lbpcascade_frontalface.xml'))
+def init():
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+    current_id = 0
+    face_cascade = cv2.CascadeClassifier(frontalface)
 
-for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        path = os.path.join(root, file)
-        label = os.path.basename(root).replace(" ", "-").lower()
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
 
-        if not label in label_ids:
-            label_ids[label] = current_id
-            current_id += 1
-        id_ = label_ids[label]
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+            id_ = label_ids[label]
 
-        # convert to gray
-        image = Image.open(path).convert("L")
+            image = Image.open(path).convert("L").resize((550, 550), Image.ANTIALIAS)
 
-        image = image.resize((550, 550), Image.ANTIALIAS)
+            image_array = np.array(image, "uint8")
+            faces = face_cascade.detectMultiScale(image_array)
+            #faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.2, minNeighbors=5)
 
-        image_array = np.array(image, "uint8")
-        faces = face_cascade.detectMultiScale(image_array)
-        #faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.2, minNeighbors=5)
+            # get region_of_interest
+            for(x_cord, y_cord, width, height) in faces:
+                region_of_interest = image_array[y_cord:y_cord + height, x_cord:x_cord + width]
+                x_train.append(region_of_interest)
+                y_labels.append(id_)
 
-        # get region_of_interest
-        for(x, y, w, h) in faces:
-            region_of_interest = image_array[y:y + h, x:x + w]
-            x_train.append(region_of_interest)
-            y_labels.append(id_)
+    # Save labels id
 
-# Save labels id
-with open("data/labels.pickle", 'wb') as f:
-    pickle.dump(label_ids, f)
+    recognizer.train(x_train, np.array(y_labels))
+    save_data()
 
-recognizer.train(x_train, np.array(y_labels))
+def save_data():
+    recognizer.save("data/trainer.yml")
+    with open("data/labels.pickle", 'wb') as f:
+        pickle.dump(label_ids, f)
+    print('Successfully :)')
 
-recognizer.save("data/trainer.yml")
-
-print('Successfully :)')
+init()
